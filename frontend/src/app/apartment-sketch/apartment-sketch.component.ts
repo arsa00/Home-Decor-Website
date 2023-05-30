ff2faeedc01b4adeed83a08f12fae282a45f5456
arsa00 <arsenije_2000@hotmail.com>
2023-05-30 17:05:04 +0200

    Implemented detection of clicking on room sketch that should be moved; Fixed bugs in collision detection; Fixed mouse coordinates to be relative to begging of canvas;

--- frontend/src/app/apartment-sketch/apartment-sketch.component.ts
+++ frontend/src/app/apartment-sketch/apartment-sketch.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApartmentSketch, RoomSketch } from '../models/ApartmentSketch';
+import { ApartmentSketch, ProgressState, RoomSketch } from '../models/ApartmentSketch';
 
 @Component({
   selector: 'app-apartment-sketch',
@@ -7,16 +7,17 @@ import { ApartmentSketch, RoomSketch } from '../models/ApartmentSketch';
   styleUrls: ['./apartment-sketch.component.css']
 })
 export class ApartmentSketchComponent implements OnInit {
+	static readonly MOVING_ROOM_FILL_COLOR: string = "#d3d3d3";
+	static readonly BORDER_FILL_OFFSET: number = 1;
 
 	static sketchCanvas;
 	static sketchCanvasContext;
+	static sketchCanvasClientRect;
 
 	static apartmentSketch?: ApartmentSketch;
 
 	static mousePosX: number;
 	static mousePosY: number;
-	static isCollisionDetected: boolean = false;
-	// static isAnyMoving: boolean = false;
 	static selectedRoomIndex: number = -1;
 	static selectedRoom?: RoomSketch;
 
@@ -25,122 +26,164 @@ export class ApartmentSketchComponent implements OnInit {
 	ngOnInit(): void {
 		ApartmentSketchComponent.sketchCanvas = document.querySelector("#apartmentCanvas");
 		ApartmentSketchComponent.sketchCanvasContext = ApartmentSketchComponent.sketchCanvas.getContext("2d");
+		ApartmentSketchComponent.sketchCanvasClientRect = ApartmentSketchComponent.sketchCanvas.getBoundingClientRect();
 
 		if(!ApartmentSketchComponent.apartmentSketch){
 			ApartmentSketchComponent.apartmentSketch = new ApartmentSketch();
+
+			ApartmentSketchComponent.apartmentSketch.roomSketches.push(new RoomSketch(150, 250, 500, 300, ProgressState.IN_PROGRESS));
+			ApartmentSketchComponent.apartmentSketch.roomSketches.push(new RoomSketch(150, 250, 100, 500, ProgressState.FINISHED));
 			ApartmentSketchComponent.apartmentSketch.roomSketches.push(new RoomSketch(150, 400));
-			ApartmentSketchComponent.apartmentSketch.roomSketches.push(new RoomSketch(150, 250, 500, 300));
-			ApartmentSketchComponent.apartmentSketch.roomSketches.push(new RoomSketch(150, 250, 100, 500));
+		}
+
+		for(let rs of ApartmentSketchComponent.apartmentSketch.roomSketches) {
+			rs.x = rs.savedX;
+			rs.y = rs.savedY;
 		}
 
 		ApartmentSketchComponent.sketchCanvas.height = "700";
 		ApartmentSketchComponent.sketchCanvas.style.height = "700px";
 		ApartmentSketchComponent.sketchCanvas.width = "800";
 		ApartmentSketchComponent.sketchCanvas.style.width = "800px";
-		ApartmentSketchComponent.drawAllTables();
+		ApartmentSketchComponent.drawAllRoomSketches();
 
 		ApartmentSketchComponent.sketchCanvas.addEventListener("mousedown", ApartmentSketchComponent.startPosition);
-		ApartmentSketchComponent.sketchCanvas.addEventListener("mousemove", ApartmentSketchComponent.moveTable);
+		ApartmentSketchComponent.sketchCanvas.addEventListener("mousemove", ApartmentSketchComponent.moveRoom);
 		ApartmentSketchComponent.sketchCanvas.addEventListener("mouseup",   ApartmentSketchComponent.endPosition);
 		ApartmentSketchComponent.sketchCanvas.addEventListener("mouseout",  ApartmentSketchComponent.endPosition);
 	}
 
-	static drawAllTables() {
+	static clearRoomSketch(rs: RoomSketch): void {
+		let bOff: number = ApartmentSketchComponent.BORDER_FILL_OFFSET;
+		ApartmentSketchComponent.sketchCanvasContext.clearRect(rs.x - bOff, rs.y - bOff, rs.width + 2*bOff, rs.height + 2*bOff);
+	}
+
+	static drawRoomSketch(rs: RoomSketch): void {
+		ApartmentSketchComponent.clearRoomSketch(rs);
+		// ApartmentSketchComponent.sketchCanvasContext.beginPath();
+
+		if(!rs.isSet) {
+			ApartmentSketchComponent.sketchCanvasContext.fillStyle = ApartmentSketchComponent.MOVING_ROOM_FILL_COLOR;
+			ApartmentSketchComponent.sketchCanvasContext.fillRect(rs.x, rs.y, rs.width, rs.height);
+
+			if(rs.isCollided) 
+				ApartmentSketchComponent.sketchCanvasContext.strokeStyle = 'red';
+			else 
+				ApartmentSketchComponent.sketchCanvasContext.strokeStyle = 'black';
+
+			ApartmentSketchComponent.sketchCanvasContext.strokeRect(rs.x, rs.y, rs.width, rs.height);
+
+			ApartmentSketchComponent.sketchCanvasContext.strokeStyle = 'black';
+			return;
+		}
+
+
+		if(rs.progress == ProgressState.IN_PROGRESS) {
+			ApartmentSketchComponent.sketchCanvasContext.fillStyle = RoomSketch.IN_PROGRESS_FILL_COLOR;
+			ApartmentSketchComponent.sketchCanvasContext.fillRect(rs.x, rs.y, rs.width, rs.height);
+		} else if(rs.progress == ProgressState.FINISHED) {
+			ApartmentSketchComponent.sketchCanvasContext.fillStyle = RoomSketch.FINISHED_FILL_COLOR;
+			ApartmentSketchComponent.sketchCanvasContext.fillRect(rs.x, rs.y, rs.width, rs.height);
+		}
+
+		ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'black';
+		ApartmentSketchComponent.sketchCanvasContext.strokeRect(rs.x, rs.y, rs.width, rs.height);
+	}
+
+	static drawAllRoomSketches(): void {
 		ApartmentSketchComponent.apartmentSketch.roomSketches.forEach((rs, roomIndex) => {
 			if(roomIndex != ApartmentSketchComponent.selectedRoomIndex) {
-				if(rs.isInvalid) {
-					ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'red';
-					ApartmentSketchComponent.sketchCanvasContext.fillRect(rs.x, rs.y, rs.width, rs.height);
-					ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'black';
-				}
-				else {
-					ApartmentSketchComponent.sketchCanvasContext.fillRect(rs.x, rs.y, rs.width, rs.height);
-				}
+				ApartmentSketchComponent.drawRoomSketch(rs);
 			}
 		});		
 	}
 
-	static startPosition(e)
-	{
-		ApartmentSketchComponent.mousePosX = e.clientX;
-		ApartmentSketchComponent.mousePosY = e.clientY;
+	static startPosition(e): void {
+		let mouseCurrX: number = e.clientX - ApartmentSketchComponent.sketchCanvasClientRect.left;
+		let mouseCurrY: number = e.clientY - ApartmentSketchComponent.sketchCanvasClientRect.top;
 
-		// TODO: find which room should move and set clickedIndex accordingly
-		let clickedIndex = 0;
-		ApartmentSketchComponent.selectedRoomIndex = clickedIndex;
-		ApartmentSketchComponent.selectedRoom = ApartmentSketchComponent.apartmentSketch.roomSketches[clickedIndex];
+		ApartmentSketchComponent.mousePosX = mouseCurrX;
+		ApartmentSketchComponent.mousePosY = mouseCurrY;
+
+		console.log(mouseCurrX, mouseCurrY);
+
+		for(let i: number = ApartmentSketchComponent.apartmentSketch.roomSketches.length - 1; i >= 0; i--) {
+			let currRS: RoomSketch = ApartmentSketchComponent.apartmentSketch.roomSketches[i];
+			if(mouseCurrX >= currRS.x && mouseCurrX <= currRS.x + currRS.width && mouseCurrY >= currRS.y && mouseCurrY <= currRS.y + currRS.height) {
+				ApartmentSketchComponent.selectedRoomIndex = i;
+				ApartmentSketchComponent.selectedRoom = currRS;
+				ApartmentSketchComponent.selectedRoom.isSet = false;
+				break;
+			}
+		}
 	}
 
-	static moveTable(e)
-	{
+	static moveRoom(e): void {
 		if(!ApartmentSketchComponent.selectedRoom) return;
 
-		ApartmentSketchComponent.sketchCanvasContext.clearRect(ApartmentSketchComponent.selectedRoom.x, ApartmentSketchComponent.selectedRoom.y, ApartmentSketchComponent.selectedRoom.width, ApartmentSketchComponent.selectedRoom.height);
-		ApartmentSketchComponent.drawAllTables();
+		let mouseCurrX: number = e.clientX - ApartmentSketchComponent.sketchCanvasClientRect.left;
+		let mouseCurrY: number = e.clientY - ApartmentSketchComponent.sketchCanvasClientRect.top;
+
+		ApartmentSketchComponent.clearRoomSketch(ApartmentSketchComponent.selectedRoom);
+		ApartmentSketchComponent.drawAllRoomSketches();
 
 		let errorX: boolean = false, errorY: boolean = false;
 		
 		// check and correct moving off the canvas
-		if(ApartmentSketchComponent.selectedRoom.x + (e.clientX - ApartmentSketchComponent.mousePosX) < 0)
+		if(ApartmentSketchComponent.selectedRoom.x + (mouseCurrX - ApartmentSketchComponent.mousePosX) < 0)
 		{
 			ApartmentSketchComponent.selectedRoom.x = 0;
 			errorX = true;
 		}
-		if(ApartmentSketchComponent.selectedRoom.x + (e.clientX - ApartmentSketchComponent.mousePosX) + ApartmentSketchComponent.selectedRoom.width > ApartmentSketchComponent.sketchCanvas.width)
+		if(ApartmentSketchComponent.selectedRoom.x + (mouseCurrX - ApartmentSketchComponent.mousePosX) + ApartmentSketchComponent.selectedRoom.width > ApartmentSketchComponent.sketchCanvas.width)
 		{
 			ApartmentSketchComponent.selectedRoom.x = ApartmentSketchComponent.sketchCanvas.width - ApartmentSketchComponent.selectedRoom.width;
 			errorX = true;
 		}
-		if(ApartmentSketchComponent.selectedRoom.y + (e.clientY - ApartmentSketchComponent.mousePosY) < 0)
+		if(ApartmentSketchComponent.selectedRoom.y + (mouseCurrY - ApartmentSketchComponent.mousePosY) < 0)
 		{
 			ApartmentSketchComponent.selectedRoom.y = 0;
 			errorY = true;
 		}
-		if(ApartmentSketchComponent.selectedRoom.y + (e.clientY - ApartmentSketchComponent.mousePosY) + ApartmentSketchComponent.selectedRoom.height > ApartmentSketchComponent.sketchCanvas.height)
+		if(ApartmentSketchComponent.selectedRoom.y + (mouseCurrY - ApartmentSketchComponent.mousePosY) + ApartmentSketchComponent.selectedRoom.height > ApartmentSketchComponent.sketchCanvas.height)
 		{
 			ApartmentSketchComponent.selectedRoom.y = ApartmentSketchComponent.sketchCanvas.height - ApartmentSketchComponent.selectedRoom.height;
 			errorY = true;
 		}
 		
-		
-		
-		if(!errorX) ApartmentSketchComponent.selectedRoom.x = ApartmentSketchComponent.selectedRoom.x + (e.clientX - ApartmentSketchComponent.mousePosX);
-		if(!errorY) ApartmentSketchComponent.selectedRoom.y = ApartmentSketchComponent.selectedRoom.y + (e.clientY - ApartmentSketchComponent.mousePosY);
+		if(!errorX) ApartmentSketchComponent.selectedRoom.x = ApartmentSketchComponent.selectedRoom.x + (mouseCurrX - ApartmentSketchComponent.mousePosX);
+		if(!errorY) ApartmentSketchComponent.selectedRoom.y = ApartmentSketchComponent.selectedRoom.y + (mouseCurrY - ApartmentSketchComponent.mousePosY);
 
+		// detect collision with other room sketches
 		for(let i = 0; i < ApartmentSketchComponent.apartmentSketch.roomSketches.length; i++) {
-			if(i == ApartmentSketchComponent.selectedRoomIndex) continue;
+			if(i == ApartmentSketchComponent.selectedRoomIndex || !ApartmentSketchComponent.apartmentSketch.roomSketches[i].isSet) continue;
 
 			let rs: RoomSketch = ApartmentSketchComponent.apartmentSketch.roomSketches[i];
 
 			if(((ApartmentSketchComponent.selectedRoom.y >= rs.y && ApartmentSketchComponent.selectedRoom.y < rs.y + rs.height) || (ApartmentSketchComponent.selectedRoom.y <= rs.y && ApartmentSketchComponent.selectedRoom.y + ApartmentSketchComponent.selectedRoom.height > rs.y)) && 
 			   ((ApartmentSketchComponent.selectedRoom.x <= rs.x && ApartmentSketchComponent.selectedRoom.x + ApartmentSketchComponent.selectedRoom.width > rs.x)  || (ApartmentSketchComponent.selectedRoom.x >= rs.x && ApartmentSketchComponent.selectedRoom.x < rs.x + rs.width))) {
-				ApartmentSketchComponent.isCollisionDetected = true;
+				ApartmentSketchComponent.selectedRoom.isCollided = true;
 				break;
 			}
 			else
-				ApartmentSketchComponent.isCollisionDetected = false;
+				ApartmentSketchComponent.selectedRoom.isCollided = false;
 		}
-		
-		if(ApartmentSketchComponent.isCollisionDetected) ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'red';
-		else ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'green';
 
-		ApartmentSketchComponent.sketchCanvasContext.fillRect(ApartmentSketchComponent.selectedRoom.x, ApartmentSketchComponent.selectedRoom.y, ApartmentSketchComponent.selectedRoom.width, ApartmentSketchComponent.selectedRoom.height);
-		ApartmentSketchComponent.sketchCanvasContext.fillStyle = 'black';
-		ApartmentSketchComponent.mousePosX = e.clientX;
-		ApartmentSketchComponent.mousePosY = e.clientY;
+		ApartmentSketchComponent.drawRoomSketch(ApartmentSketchComponent.selectedRoom);
+		ApartmentSketchComponent.mousePosX = mouseCurrX;
+		ApartmentSketchComponent.mousePosY = mouseCurrY;
 	}
 
-	static endPosition()
-	{ // ovo se takodje treba generalizovati
+	static endPosition(): void { // ovo se takodje treba generalizovati
 		if(ApartmentSketchComponent.selectedRoom) {
-			if(ApartmentSketchComponent.isCollisionDetected) ApartmentSketchComponent.selectedRoom.isInvalid = true;
-			else ApartmentSketchComponent.selectedRoom.isInvalid = false;
+			if(ApartmentSketchComponent.selectedRoom.isCollided) ApartmentSketchComponent.selectedRoom.isSet = false;
+			else ApartmentSketchComponent.selectedRoom.isSet = true; // TODO: update savedX & savedY of selected room
 		}
 
 		ApartmentSketchComponent.selectedRoom = undefined;
 		ApartmentSketchComponent.selectedRoomIndex = -1;
 
-		ApartmentSketchComponent.drawAllTables();
+		ApartmentSketchComponent.drawAllRoomSketches();
 	}
 
 }
--- frontend/src/app/models/ApartmentSketch.ts
+++ frontend/src/app/models/ApartmentSketch.ts
@@ -6,25 +6,29 @@ enum ProgressState {
 
 class RoomSketch {
     static readonly FINISHED_FILL_COLOR: string = "#a6fa57";
-    static readonly UNFINISHED_FILL_COLOR: string = "#fa5757";
+    static readonly IN_PROGRESS_FILL_COLOR: string = "#fa5757";
 
     height: number;
     width: number;
+    savedX: number;
+    savedY: number;
     x: number;
     y: number;
     progress: ProgressState;
+    isCollided: boolean;
 
-    // isMoving?: boolean = false;
-    isInvalid?: boolean = false
+    isSet?: boolean = false;
 
-    constructor(height: number, width: number, x: number = 0, y: number = 0, isFilled: boolean = false, progress = ProgressState.NOT_STARTED) {
+
+    constructor(height: number, width: number, savedX: number = 0, savedY: number = 0, progress = ProgressState.NOT_STARTED) {
         this.height = height;
         this.width = width;
-        this.x = x;
-        this.y = y;
+        this.savedX = savedX;
+        this.savedY = savedY;
         this.progress = progress;
 
-        this.isInvalid = false;
+        this.isSet = true;
+        this.isCollided = false;
     }
 }
 
