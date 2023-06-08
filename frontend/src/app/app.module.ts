a3aecbd597b47e99660cfa8eff92947e90824bc9
arsa00 <arsenije_2000@hotmail.com>
2023-06-08 06:12:02 +0200

    Added Bootstrap and Angular Material modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApartmentSketchComponent } from './apartment-sketch/apartment-sketch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ApartmentSketchComponent } from './apartment-sketch/apartment-sketch.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
