6f2f99d5331e6143702070f9c069c4b445d42d57
arsa00 <arsenije_2000@hotmail.com>
2023-06-09 23:26:52 +0200

    [backend] Initial server setup

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,7 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const PORT = 4000;
 const app = (0, express_1.default)();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(4000, () => console.log(`Express server running on port 4000`));
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+mongoose_1.default.connect("mongodb://127.0.0.1:27017/test");
+const connection = mongoose_1.default.connection;
+connection.once("open", () => {
+    console.log("DB connected");
+});
+const router = express_1.default.Router();
+// router.use("/user", userRouter);
+router.get("/", (req, res) => { res.send("Server working..."); });
+app.use("/", router);
+app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
 //# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const PORT: number = 4000;
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(4000, () => console.log(`Express server running on port 4000`));
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("DB connected");
+})
+
+const router = express.Router();
+// router.use("/user", userRouter);
+router.get("/", (req, res) => { res.send("Server working..."); });
+
+app.use("/", router);
+app.listen(PORT, () => console.log(`Express server running on port ${ PORT }`));
