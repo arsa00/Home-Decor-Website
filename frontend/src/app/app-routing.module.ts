bd91aead1382f4babe6b91f723a92c9575a965b4
arsa00 <arsenije_2000@hotmail.com>
2023-06-11 06:52:47 +0200

    [fronted] Added register to routing module; Configured links between login and register pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { ClientProfileComponent } from './client-profile/client-profile.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { GuestPageComponent } from './guest-page/guest-page.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "adminLogin", component: AdminLoginComponent },
+  { path: "register", component: RegisterComponent },
   { path: "clientProfile", component: ClientProfileComponent },
   { path: "guestPage", component: GuestPageComponent },
 ];
