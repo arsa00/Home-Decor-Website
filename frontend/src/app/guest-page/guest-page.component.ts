4b59965c03eda2f3943889caec4f81f1ce4ed951
arsa00 <arsenije_2000@hotmail.com>
2023-06-17 09:34:59 +0200

    [frontend] Finished design of 'Guest Page'; Added mock data for agencies; Implemented sorting agencies on client side

--- frontend/src/app/guest-page/guest-page.component.ts
+++ frontend/src/app/guest-page/guest-page.component.ts
@@ -8,11 +8,58 @@ import { User } from '../models/User';
 })
 export class GuestPageComponent implements OnInit {
 
-  agencySearchResults: User[];
+  static agency1: User = {
+    username: "Arsa",
+    password: "123",
+    type: "agency",
+    mail: "mail@m.com",
+    jwt: "jwt token",
+    name: "Naziv agencije 1",
+    description: "Opis agencije koji moze biti poduzi i u \n par redova",
+    address: "Adresa agencije 22",
+    // imageType: ".png"
+  }
+
+  static agency2: User = {
+    username: "Arsa",
+    password: "123",
+    type: "agency",
+    mail: "mail@m.com",
+    jwt: "jwt token",
+    name: "Naziv agencije 2",
+    description: "Opis agencije koji moze biti poduzi i u \n par redova",
+    address: "Adresa agencije 21",
+    imageType: ".png"
+  }
+
+  agencySearchResults: User[] = [GuestPageComponent.agency1, GuestPageComponent.agency1, GuestPageComponent.agency1, GuestPageComponent.agency2, GuestPageComponent.agency2 ];
+  searchParam: string = "name";
+  searchTerm: string;
+
+  sortParam: string = "name";
+  sortDir: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(): void {
+
+  }
+
+  sort(): void {
+    this.agencySearchResults.sort((agency1: User, agency2: User) => {
+      if(this.sortParam == "name") {
+        if(agency1.name < agency2.name) return -this.sortDir;
+        else if(agency1.name === agency2.name) return 0;
+        else return this.sortDir;
+      } else {
+        if(agency1.address < agency2.address) return -this.sortDir;
+        else if(agency1.address === agency2.address) return 0;
+        else return this.sortDir;
+      }
+    })
   }
 
 }
