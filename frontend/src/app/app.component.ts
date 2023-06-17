89c46172b83d1edc1b216a157f8f86bcaee7736c
arsa00 <arsenije_2000@hotmail.com>
2023-06-17 06:02:39 +0200

    [frontend] Created automatic removal of cached user's data after his JWT expires; Change title and icon of website

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from './global-constants';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PIA Rocks';
+export class AppComponent implements OnInit {
+  
+  title = 'Home Decor';
+
+  ngOnInit(): void {
+    const jwtHelper = new JwtHelperService();
+    let user: User;
+
+    try {
+      user = JSON.parse(localStorage.getItem(GlobalConstants.LOCAL_STORAGE_LOGGED_USER));
+    } catch(err) {
+      return;
+    }
+    
+    if(!user) return;
+
+    if(jwtHelper.isTokenExpired(user.jwt)) {
+      localStorage.removeItem(GlobalConstants.LOCAL_STORAGE_LOGGED_USER);
+    }
+
+  }
 }
