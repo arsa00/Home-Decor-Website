e24c9682b4322e3f8aa280c5376434f6fe64c3e5
arsa00 <arsenije_2000@hotmail.com>
2023-07-09 16:45:54 +0200

    [frontend] Added aditional feedback information to user within 'Agency Employees' page, 'Admin - Agency Employees' page and 'Admin User List' page as well

--- frontend/src/app/admin-agency-employees/admin-agency-employees.component.ts
+++ frontend/src/app/admin-agency-employees/admin-agency-employees.component.ts
@@ -3,6 +3,9 @@ import { AgencyRequest } from '../models/AgencyRequest';
 import { AgencyService } from '../services/agency.service';
 import { User } from '../models/User';
 import { GlobalConstants } from '../global-constants';
+import * as bootstrap from 'bootstrap';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-agency-employees',
@@ -16,7 +19,12 @@ export class AdminAgencyEmployeesComponent implements OnInit {
 
   allRequests: AgencyRequest[] = [];
 
-  constructor(private agencyService: AgencyService) { }
+  errToastMsg: string;
+  succToastMsg: string;
+
+  constructor(private router: Router,
+              private agencyService: AgencyService,
+              private userService: UserService) { }
 
   ngOnInit(): void {
     this.loggedAdmin = JSON.parse(localStorage.getItem(GlobalConstants.LOCAL_STORAGE_LOGGED_ADMIN));
@@ -33,6 +41,16 @@ export class AdminAgencyEmployeesComponent implements OnInit {
     });
   }
 
+  displayErrorToast(msg: string) {
+    this.errToastMsg = msg;
+    new bootstrap.Toast(document.getElementById("err0")).show(); 
+  }
+
+  displaySuccessfulToast(msg: string) {
+    this.succToastMsg = msg;
+    new bootstrap.Toast(document.getElementById("succ0")).show(); 
+  }
+
   acceptReq(index: number) {
     const req = this.allRequests[index];
 
@@ -42,10 +60,13 @@ export class AdminAgencyEmployeesComponent implements OnInit {
         this.agency = updatedAgency;
         localStorage.setItem(GlobalConstants.LOCAL_STORAGE_LOGGED_USER, JSON.stringify(updatedAgency));
         // this.allRequests.splice(index, 1);
-        location.reload();
+        this.displaySuccessfulToast("Zahtev uspešno prihvaćen.");
+        setTimeout(() => {
+          location.reload();
+        }, 750);
       },
       error: () => {
-
+        this.displayErrorToast("Došlo je do greške. Pokušajte ponovo.");
       }
     });
   }
@@ -56,10 +77,24 @@ export class AdminAgencyEmployeesComponent implements OnInit {
     this.agencyService.rejectAgencyRequest(this.loggedAdmin.jwt, req._id).subscribe({
       next: (updatedAgency: User) => {
         this.allRequests.splice(index, 1);
+        this.displaySuccessfulToast("Zahtev uspešno odbijen.");
       },
       error: () => {
-
+        this.displayErrorToast("Došlo je do greške. Pokušajte ponovo.");
       }
     });
+  }
+
+  returnToHomePage() {
+    this.router.navigate([GlobalConstants.ROUTE_ADMIN_DASHBOARD]);
+  }
+
+  returnToUserList() {
+    this.router.navigate([GlobalConstants.ROUTE_ADMIN_USER_LIST]);
+  }
+
+  logoutAdmin() {
+    this.userService.logout();
+    this.router.navigate([GlobalConstants.ROUTE_LOGIN]);
   }
 }
